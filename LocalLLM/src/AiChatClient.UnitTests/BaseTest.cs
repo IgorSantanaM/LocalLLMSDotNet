f62d6b037b7a2df7cf0200ae4102c0529dd9d3b8
Igor Santana Medeiros <1434@ucdb.br>
2026-06-30 13:07:25 -0400

    (feat): Added Equivalence evaluator for unit testing

---
+++ LocalLLM/src/AiChatClient.UnitTests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+namespace AiChatClient.UnitTests
+{
+    public abstract class BaseTest
+    {
+        readonly Lazy<IChatClient> _chatClientHolder = new(CreateOllamaChatClient);
+        protected IChatClient ChatClient  => _chatClientHolder.Value;
+
+        [TearDown]
+        protected virtual void TearDown()
+        {
+            if (_chatClientHolder.IsValueCreated)
+                ChatClient.Dispose();
+        }
+
+        static IChatClient CreateOllamaChatClient()
+        {
+            const string modelId = "qwen3.5";
+
+            var ollamaClient = new OllamaApiClient("http://127.0.0.1:11434", modelId);
+
+            return new ChatClientBuilder(ollamaClient)
+                .UseFunctionInvocation()
+                .Build();
+        }
+    }
+}
---
+++ LocalLLM/src/AiChatClient.UnitTests/ChatClientServicesTests.cs
@@ -0,0 +1,56 @@
+﻿using AiChatClient.Maui.Services;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.AI.Evaluation;
+using Microsoft.Extensions.AI.Evaluation.Quality;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiChatClient.UnitTests
+{
+    public class ChatClientServicesTests : BaseTest
+    {
+        [Test]
+        public async Task MaintainConversationHistory_EquivalenceEvaluator()
+        {
+            // Arrange
+            var service = new ChatClientServices(ChatClient);
+            var equivalenceEvaluator = new EquivalenceEvaluator();
+            var equivalenceContext = new EquivalenceEvaluatorContext("The users favorite color is blue");
+
+            const string firstPrompt = "My favorite color is blue. Remeber this.";
+            const string secondPrompt = "What is my favorite color?";
+            string firstAssistantResponse = string.Empty;
+            string secondAssistantResponse = string.Empty;
+
+            // Act
+            await foreach (var response in service.GetStreamingResponseAsync(
+                                new ChatMessage(ChatRole.User, firstPrompt), null, CancellationToken.None))
+            {
+                firstAssistantResponse += response.Text;
+            }
+
+            service.AddAssistantResponse(firstAssistantResponse);
+
+            await foreach (var response in service.GetStreamingResponseAsync(
+                                new ChatMessage(ChatRole.User, secondPrompt), null, CancellationToken.None))
+            {
+                secondAssistantResponse += response.Text;
+            }
+
+            var responseForEvaluation = new ChatResponse(new ChatMessage(ChatRole.Assistant, secondAssistantResponse));
+            var chatMessageForEvaluator = new ChatMessage(ChatRole.User, secondPrompt);
+            
+            var equivalenceResult = await equivalenceEvaluator.EvaluateAsync(
+                [chatMessageForEvaluator],
+                responseForEvaluation,
+                new(ChatClient),
+                [equivalenceContext]);
+
+            var equivalenceResultMetric = equivalenceResult.Get<NumericMetric>(EquivalenceEvaluator.EquivalenceMetricName);
+            // Assert
+            Assert.That(equivalenceResultMetric.Value, Is.GreaterThanOrEqualTo(4));
+
+        }
+    }
+}
