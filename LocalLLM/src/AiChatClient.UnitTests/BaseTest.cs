8e32bbcec8dd3b2b01fc97d6764fd71bbd7cadeb
Igor Santana Medeiros <1434@ucdb.br>
2026-06-30 16:55:21 -0400

    (feat): Added tests for checking documents ingestion

--- LocalLLM/src/AiChatClient.UnitTests/BaseTest.cs
+++ LocalLLM/src/AiChatClient.UnitTests/BaseTest.cs
@@ -4,18 +4,30 @@ using OllamaSharp;
 
 namespace AiChatClient.UnitTests
 {
-    public abstract class BaseTest
+    public abstract class BaseTest 
     {
+        readonly Lazy<IEmbeddingGenerator<string, Embedding<float>>> _embeddingGeneratorHolder = new(CreateOllamaEmbeddingGenerator);
         readonly Lazy<IChatClient> _chatClientHolder = new(CreateOllamaChatClient);
         readonly Lazy<GitHubClient> _gitHubClientHolder = new(new GitHubClient(new ProductHeaderValue("AiChatClient")));
         protected IChatClient ChatClient  => _chatClientHolder.Value;
         protected GitHubClient GitHubClient => _gitHubClientHolder.Value;
+        protected IEmbeddingGenerator<string, Embedding<float>> EmbeddingGenerator => _embeddingGeneratorHolder.Value;
 
-        [TearDown]
+        [OneTimeTearDown]
         protected virtual void TearDown()
         {
             if (_chatClientHolder.IsValueCreated)
                 ChatClient.Dispose();
+
+            if (_embeddingGeneratorHolder.IsValueCreated)
+                EmbeddingGenerator.Dispose();
+        }
+
+        private static IEmbeddingGenerator<string, Embedding<float>> CreateOllamaEmbeddingGenerator()
+        {
+            const string modelId = "qwen3-embedding";
+
+            return new OllamaApiClient("http://127.0.0.1:11434", modelId);
         }
 
         static IChatClient CreateOllamaChatClient()
---
+++ LocalLLM/src/AiChatClient.UnitTests/PdfIngestionServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using AiChatClient.Maui.Models;
+using AiChatClient.Maui.Services;
+using Microsoft.Extensions.VectorData;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Core;
+using UglyToad.PdfPig.Writer;
+
+namespace AiChatClient.UnitTests
+{
+    public class PdfIngestionServiceTests : BaseTest
+    {
+        [Test]
+        public async Task SearchAsync_ReturnsExpectedText_WhenScoresAreWithingThresholds()
+        {
+            // Arrange
+            const string pdfText = "The quick brown fox jumps over the lazy dog";
+
+            var vectorCollection = CreateVectorStoreCollection();
+            var pdfIngestionService = new PdfIngestionService(vectorCollection, EmbeddingGenerator);
+
+
+            var pdfStream = CreatePdfStream(pdfText);
+            await pdfIngestionService.IngestPdfAsync(pdfStream, "foxes.pdf");
+
+            // Act
+            var result = await pdfIngestionService.SearchAsync(pdfText);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.Contain(pdfText));
+        }
+
+        static VectorStoreCollection<string, PdfChunkRecord> CreateVectorStoreCollection()
+        {
+            var vectorStore = new InMemoryVectorStore();
+            return vectorStore.GetCollection<string, PdfChunkRecord>("test-pdf-chunks");
+        }
+        static Stream CreatePdfStream(string text)
+        {
+            var builder = new PdfDocumentBuilder();
+            var page = builder.AddPage(PageSize.A4);
+            var font = builder.AddStandard14Font(UglyToad.PdfPig.Fonts.Standard14Fonts.Standard14Font.Helvetica);
+            page.AddText(text, 12, new PdfPoint(25, 700), font);
+
+            var bytes = builder.Build();
+            return new MemoryStream(bytes);
+        }
+    }
+}
